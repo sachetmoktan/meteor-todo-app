4afb1f3582e69b97c93854d90fc87299e5a5a383
sachet-moktan <sachet.moktan@asterdio.com>
2025-10-31 20:54:04 +0545

    feat: added todo feat with login functionality

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "../api/TasksCollection.js";
+import { Task } from "./Task.jsx";
+import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import { TaskForm } from "./TaskForm.jsx";
+import { LoginForm } from "./LoginForm.jsx";
 
-export const App = () => (
-  <div>
-    <h1>Todo App</h1>
-  </div>
-);
+export const App = () => {
+  const isLoading = useSubscribe("tasks");
+
+  const user = useTracker(() => Meteor.user());
+  const logout = () => Meteor.logout();
+
+  const [hideCompleted, setHideCompleted] = React.useState(false);
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const tasks = useTracker(() => {
+    if (!user) {
+      return [];
+    }
+    return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  });
+
+  const pendingTasksCount = useTracker(() => {
+    if (!user) {
+      return 0;
+    }
+    return TasksCollection.find(hideCompletedFilter).count();
+  });
+
+  const handleToggleChecked = (data) => {
+    return Meteor.callAsync("tasks.toggleChecked", {
+      _id: data._id,
+      isChecked: data.isChecked,
+    });
+  };
+  const onDeleteClick = (_id) => {
+    return Meteor.callAsync("tasks.delete", _id);
+  };
+
+  if (isLoading()) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <h1>
+        📝️ To Do List {`${pendingTasksCount ? ` (${pendingTasksCount})` : ""}`}
+      </h1>
+
+      <div className="app">
+        <header>
+          <div className="app-bar">
+            <div className="app-header">
+              <h1>Welcome to Meteor!</h1>
+            </div>
+          </div>
+        </header>
+
+        <div className="main">
+          {user ? (
+            <Fragment>
+              <div className="user" onClick={logout}>
+                {user.username} 🚪
+              </div>
+              <TaskForm />
+
+              <div className="filter">
+                <button onClick={() => setHideCompleted(!hideCompleted)}>
+                  {hideCompleted ? "Show All" : "Hide Completed"}
+                </button>
+              </div>
+
+              <ul className="tasks">
+                {tasks.map((task) => (
+                  <Task
+                    key={task._id}
+                    task={task}
+                    onCheckboxClick={handleToggleChecked}
+                    onDeleteClick={onDeleteClick}
+                  />
+                ))}
+              </ul>
+            </Fragment>
+          ) : (
+            <LoginForm />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
--- server/main.js
+++ server/main.js
@@ -1,4 +1,24 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { TasksCollection } from "../imports/api/TasksCollection";
+import "../imports/api/TasksPublications";
+import "../imports/api/tasksMethods";
+
+const SEED_USERNAME = "admin";
+const SEED_PASSWORD = "admin";
+
+// const insertTask = (taskText, user) =>
+//   TasksCollection.insertAsync({
+//     text: taskText,
+//     userId: user._id,
+//     createdAt: new Date(),
+//   });
 
 Meteor.startup(async () => {
+  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+    await Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
 });
